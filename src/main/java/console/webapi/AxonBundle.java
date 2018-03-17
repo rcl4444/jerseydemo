8c2d836fe7024ae619f4766a0aa210416391999d
rcl4444 <1043340906@qq.com>
2018-03-18 02:07:12 +0800

    1

--- src/main/java/console/webapi/AxonBundle.java
+++ src/main/java/console/webapi/AxonBundle.java
@@ -11,13 +11,13 @@ import io.dropwizard.setup.Environment;
 public class AxonBundle<T extends io.dropwizard.Configuration> implements ConfiguredBundle<T> {
 
 	protected Configurer configurer;
-	
+
 	protected Configuration configuration;
-	
-	public Configuration getConfiguration(){
+
+	public Configuration getConfiguration() {
 		return this.configuration;
 	}
-	
+
 	@Override
 	public void run(T configuration, Environment environment) throws Exception {
 		this.configuration = this.configurer.buildConfiguration();
@@ -26,6 +26,12 @@ public class AxonBundle<T extends io.dropwizard.Configuration> implements Config
 	@Override
 	public void initialize(Bootstrap<?> bootstrap) {
 		this.configurer = DefaultConfigurer.defaultConfiguration();
+		configurer.registerCommandHandler(c -> new MyAnnotatedHandler());
+		EventHandlingConfiguration ehConfiguration = new EventHandlingConfiguration()
+				.registerEventHandler(conf -> new MyEventHandlerClass());
+
+		// the module needs to be registered with the Axon Configuration
+		configurer.registerModule(ehConfiguration);
 	}
 
 }
